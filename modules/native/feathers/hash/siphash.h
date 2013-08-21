4aceea1f643ad1d8cc0b553f1277e93155a3623d
Steven Oliver <oliver.steven@gmail.com>
2013-08-20 21:21:43 -0400

    More WIP toward siphash

--- modules/native/feathers/hash/siphash.h
+++ modules/native/feathers/hash/siphash.h
@@ -21,31 +21,29 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef SIPHASH_H
-#define SIPHASH_H
+#ifndef _SIPHASH_H_
+#define _SIPHASH_H_
 
-extern "C" {
 
 #include <stdlib.h>
 #include <stdint.h>
-#include <inttypes.h>
 
-        typedef struct sip_hash_st sip_hash;
+#include "hash_defs.h"
+
+typedef struct sip_hash_st sip_hash;
 
 #ifndef HAVE_UINT64_T
-        typedef struct {
-                uint32_t u32[2];
-        } sip_uint64_t;
-#define uint64_t sip_uint64_t
+typedef struct {
+        word32 u32[2];
+} sip_uint64_t;
+#define word64 sip_uint64_t
 #endif
 
-        sip_hash *sip_hash_new(uint8_t key[16], int c, int d);
-        int sip_hash_update(sip_hash *h, uint8_t *data, size_t len);
-        int sip_hash_final(sip_hash *h, uint8_t **digest, size_t *len);
-        int sip_hash_digest(sip_hash *h, uint8_t *data, size_t data_len, uint8_t **digest, size_t *digest_len);
+sip_hash *sip_hash_new(byte key[16], int c, int d);
+int sip_hash_update(sip_hash *h, byte *data, size_t len);
+int sip_hash_final(sip_hash *h, byte **digest, size_t *len);
+int sip_hash_digest(sip_hash *h, byte *data, size_t data_len, byte **digest, size_t *digest_len);
 
-        uint64_t sip_hash24(uint8_t key[16], uint8_t *data, size_t len);
+uint64_t sip_hash24(byte key[16], byte *data, size_t len);
 
 }
-
-#endif
